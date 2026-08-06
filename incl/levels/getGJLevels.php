7cbb4b806f976726c73bb41f4e3e75225c75ff21
Axton10YT <axtonsshop@gmail.com>
2026-08-05 20:16:38 -0700

    Update getGJLevels.php

--- incl/levels/getGJLevels.php
+++ incl/levels/getGJLevels.php
@@ -2,7 +2,7 @@
 chdir(dirname(__FILE__));
 
 include "../lib/connection.php";
-require_once "../lib/GJPCheck.php";
+require_once "../lib/GJPCheck.php"; // replacing with GJP3 soon. 
 require_once "../lib/exploitPatch.php";
 require_once "../lib/mainLib.php";
 require_once "../lib/generateHash.php";
